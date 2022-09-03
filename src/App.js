a7ff070944f5a164caf5397534e9835a314125c8
Khalil Attalla Firdia <khalilattalla2209@gmail.com>
2022-09-03 09:55:14 +0700

    make example for paragraph and hidden value

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-class App extends Component{
-    render(){
+// class RegularClass{}
+
+// class ComponentClass extends Component{}
+
+// const regularClassInstance = new RegularClass()
+// const componentClassInstance = new ComponentClass()
+
+// console.log("regularClassInstance", regularClassInstance);
+// console.log("componentClassInstance", componentClassInstance);
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = { displayedBio: false };
+    }
+
+    render() {
+        const bio = this.state.displayedBio ? (
+            <div>
+                <p>I Live in San Francisco, and code every daya</p>
+                <p>My Favorite language is Javascript and Goalng, and i thing React.js is awesome</p>
+                <p>Besides Coding, i also love music and Ramen :D</p>
+            </div>
+        ) : null;
+
         return (
             <div>
                 <h1>Helo!</h1>
                 <p>My name is Khalil. I'm a software engineer</p>
                 <p>I'm always looking forward to working on meaningful projects</p>
+                {bio}
             </div>
         )
     }
